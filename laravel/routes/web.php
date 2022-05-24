24c8656de9c413138c41b9bf31c32e6a8e5b0216
dibss <ludovicad01@gmail.com>
2022-05-24 16:40:31 +0200

    created files and routes for each comic

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -1,3 +1,7 @@
+<!-- Create una pagina di dettaglio per visualizzare tutte le informazioni di un fumetto.
+Definite quindi una rotta che avrà un parametro per poter visualizzare dinamicamente tutte le pagine di dettaglio.
+Infine, fate sì che cliccando sulla card di un fumetto si possa accedere alla relativa pagina di dettaglio. -->
+
 <?php
 
 use Illuminate\Support\Facades\Route;
@@ -19,9 +23,82 @@ Route::get('/', function () {
 
     $comics = Config::get('comics');
 
-    return view('comics', $comics);
+    return view('comics.comics', $comics);
     
 })->name('comics');
+
+// PARTIALS COMICS
+Route::get('/comics/action-comics-1000-the-deluxe-edition', function() {
+
+    return view('comics.partials.action-comics_1000-de');
+
+})->name('action-comics');
+
+Route::get('/comics/american-vampire-1976-1', function() {
+
+    return view('comics.partials.american-vampire-1976_1');
+
+})->name('american-vampire-1976_1');
+
+Route::get('/comics/aquaman-4-underworld', function() {
+
+    return view('comics.partials.aquaman_4-underworld');
+
+})->name('aquaman_4-underworld');
+
+Route::get('/comics/batgirl-1', function() {
+
+    return view('comics.partials.batgirl_1');
+
+})->name('batgirl_1');
+
+Route::get('/comics/batman-56', function() {
+
+    return view('comics.partials.batman_56');
+
+})->name('batman_56');
+
+Route::get('/comics/batman-beyond-1', function() {
+
+    return view('comics.partials.batman-beyond_1');
+
+})->name('batman-beyond_1');
+
+Route::get('/comics/batman-superman-1', function() {
+
+    return view('comics.partials.batman-superman_1');
+
+})->name('batman-superman_1');
+
+Route::get('/comics/batman-superman-annual-1', function() {
+
+    return view('comics.partials.batman-superman-annual_1');
+
+})->name('batman-superman-annual_1');
+
+Route::get('/comics/batman-the-joker-war-zone-1', function() {
+
+    return view('comics.partials.batman-the-joker-war-zone_1');
+
+})->name('batman-the-joker-war-zone_1');
+
+Route::get('/comics/batman-three-jokers-1', function() {
+
+    return view('comics.partials.batman-three-jokers_1');
+
+})->name('batman-three-jokers_1');
+
+Route::get('/comics/batman-white-knight-presents-harley-quinn-1', function() {
+
+    return view('comics.partials.batman-white-knight-presents-harley-quinn_1');
+
+})->name('batman-white-knight-presents-harley-quinn_1');
+
+Route::get('/comics/catwoman-1-copycats', function() {
+
+    return view('comics.partials.catwoman_1-copycats');
+
+})->name('catwoman_1-copycats');
 
 
 // SUBPAGES
