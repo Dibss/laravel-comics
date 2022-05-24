8e57f792a2e3316775d6ac52e162f7c449fa67c8
dibss <ludovicad01@gmail.com>
2022-05-24 19:27:58 +0200

    init page for single comic

---
+++ laravel/resources/views/comics/partials/comic.blade.php
@@ -0,0 +1,24 @@
+@extends('layout.layout')
+
+@section('title', 'Comic')
+
+@section('nav-links')
+  <ul>
+    <li><a href="{{ route('characters') }}">Characters</a></li>
+    <li><a href="{{ route('comics') }}" class="active">Comics</a></li>
+    <li><a href="{{ route('movies') }}">Movies</a></li>
+    <li><a href="{{ route('tv') }}">Tv</a></li>
+    <li><a href="{{ route('games') }}">Games</a></li>
+    <li><a href="{{ route('collectibles') }}">Collectibles</a></li>
+    <li><a href="{{ route('videos') }}">Videos</a></li>
+    <li><a href="{{ route('fans') }}">Fans</a></li>
+    <li><a href="{{ route('news') }}">News</a></li>
+    <li><a href="#">Shop</a></li>
+  </ul>
+@endsection
+
+@section('main-content')
+  <div class="comic-page">
+    
+  </div>
+@endsection
